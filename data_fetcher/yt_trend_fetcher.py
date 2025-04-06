3840bd5c7f35913d286872fbc338d315e975d266
Shriya-20 <shriya.mitmpl2022@learner.manipal.edu>
2025-04-06 15:53:58 +0530

    Initial commit

---
+++ data_fetcher/yt_trend_fetcher.py
@@ -0,0 +1,22 @@
+
+from googleapiclient.discovery import build
+
+API_KEY = "AIzaSyDii0e5q-U-YI_bqRghuaxpq1BGX0J20Zg"
+youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+# part="snippet,statistics": This specifies that the response should include both the video snippet (title, description, thumbnails, etc.) and statistics (like views, likes, dislikes, etc.).
+# chart="mostPopular": This specifies that the videos should be retrieved based on their popularity.
+# regionCode="IN": This specifies that the videos should be retrieved from India.
+#  function then executes the request and returns the response from the API
+def get_trending_videos():
+    request = youtube.videos().list(
+        part="snippet,statistics",
+        chart="mostPopular",
+        regionCode="IN",
+        maxResults=10
+    )
+    return request.execute()
+
+# Test the API
+print(get_trending_videos()['items'][0]['snippet']['title'])
+print(get_trending_videos()['items'][1]['snippet']['title'])
